d810623880f43c9f1eae11b5f902393db0a75455
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2025-10-30 18:08:30 +0000

    add email notif

--- run_med_vlm.sh
+++ run_med_vlm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --mem=16G
-#SBATCH --cpus-per-task=4
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
+#SBATCH --mem=82G
+#SBATCH --mail-user=sureshanikesh@yahoo.co.uk
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load Anaconda3/2022.05
