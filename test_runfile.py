9ddb2d290b00b615754bc6c2259e7e1b732bdd06
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2025-10-30 14:34:51 +0000

    Success, removing test files now

--- test_runfile.py
+++
@@ -1,5 +0,0 @@
-output_text = "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-
-print("Hopefully this is in the output log")
-with open("demofile.txt", "w") as file:
-    file.write(output_text)
--- test_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --output=output.%j.test.out
-#SBATCH --job-name=FIRST_SCRIPT_TEST_RUN
-
-python test_runfile.py
