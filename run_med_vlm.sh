f4314eb7a2747bbe0ebd113907ce56320fe82579
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2025-10-30 14:57:26 +0000

    Testing to run batch script of inferenced model

---
+++ run_med_vlm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+python medvlm.py
