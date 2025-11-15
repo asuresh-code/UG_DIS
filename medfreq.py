76a367d37235336090814d33c5a4cf90d3c5b537
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2025-11-15 15:02:40 +0000

    shapes

--- medfreq.py
+++ medfreq.py
@@ -151,7 +151,7 @@ for message in messages:
     image_tensor = transform(image_input[0])
     freq_image_tensor = torch.fft.fft2(image_tensor[0]).float().clone().detach().to(device).requires_grad_(True)
     inversed_tensor = torch.fft.ifft2(freq_image_tensor).real
-    print(inversed_tensor.shape)
+    print("1:", inversed_tensor.shape)
     grey_image_tensor = inversed_tensor.unsqueeze(0)
     image_tensor = grey_image_tensor.repeat(3,1,1)
     lower_bound_image_tensor = grey_image_tensor.clone().detach() - total_budget
@@ -227,14 +227,14 @@ for i in range(iterations):
         frequency_image_tensors[x].grad = None
 
         base_signed_pix = torch.fft.ifft2(signed_grad_freq).real
-        print(base_signed_pix.shape)
+        print("2:", base_signed_pix.shape)
 
         factor = 2.0 / torch.max(base_signed_pix)
 
         new_freq_tensor = frequency_image_tensors[x].clone().detach() + signed_grad_freq*factor
 
         adv_image = torch.fft.ifft2(new_freq_tensor).real
-        print(adv_image.shape)
+        print("3:", adv_image.shape)
 
         lower_bound_pos = torch.lt(adv_image, lower_bound_budgets[x])
         non_lower_bound_pos = torch.logical_not(lower_bound_pos)
@@ -256,8 +256,8 @@ for i in range(iterations):
 
         frequency_image_tensors[x] = torch.fft.fft2(final_image).float().clone().detach().to(device).requires_grad_(True)
         inversed_tensor = torch.fft.ifft2(frequency_image_tensors[x]).real
-        print(inversed_tensor.shape)
-        grey_image_tensors[x] = inversed_tensor.unsqueeze(0)
+        print("4:", inversed_tensor.shape)
+        grey_image_tensors[x] = inversed_tensor
         image_inputs[x] = grey_image_tensors[x].clone().repeat(3,1,1)
 
     print("Success Rate:",successes/len(generated_ids))
