eda5a233b13cab73168c2ed238a78d488212c3dd
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2025-11-18 17:04:49 +0000

    better results logging

--- testing.py
+++ testing.py
@@ -1,29 +1,10 @@
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+import numpy as np
 
+tensor = torch.tensor([3,234,3,1])
 
-image = "../../images_practice/mrabd005680.png"
-img = Image.open(image)
+newt = torch.topk(tensor, 4)
 
-transform = transforms.PILToTensor()
-img_tensor = transform(img)
-
-
-grey_image = img_tensor[0]
-grey_image = grey_image[None, :, :]
-
-grey_copy = grey_image.clone().detach().to(torch.uint8)
-transform = transforms.ToPILImage()
-grey_pil = transform(grey_copy)
-
-
-save = grey_pil.save("grey_image.png")
-
-
-colour = grey_image.repeat(3,1,1)
-
-color_pil = transform(colour)
-
-
-save = color_pil.save("rgb_image.png")
+print(sum(newt[0]))
